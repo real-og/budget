9719ec04c9ec517bb5fb8566616909d722a33762
Evgeny Grakovich <grakovich-e@yandex.by>
2022-12-13 02:25:17 +0300

    add value from the wab

--- main.py
+++ main.py
@@ -1,15 +1,30 @@
 import os
 from flask import Flask, render_template, request, flash, redirect, url_for
 from db import *
+import datetime
 
-
+def is_digit(string):
+    if string.isdigit():
+       return True
+    else:
+        try:
+            float(string)
+            return True
+        except ValueError:
+            return False
 
 
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/", methods=["POST", "GET"])
 def index():
+    if request.method == 'POST':
+        amount = request.form['amount']
+        cat = request.form['cat']
+        date = request.form['date']
+        if is_digit(amount):
+            add_expense(cat, date, amount)
     expenses = get_data()
     return render_template('page.html', exps=expenses)
 
