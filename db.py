410b004b5ef8307845ae798daf2d8b297b675f4e
Evgeny Grakovich <grakovich-e@yandex.by>
2022-12-13 04:07:47 +0300

    demo

--- db.py
+++ db.py
@@ -1,4 +1,5 @@
 from csv import writer, DictReader
+from datetime import datetime
   
 
 def add_expense(name, date, value):
@@ -10,3 +11,24 @@ def add_expense(name, date, value):
 def get_data() -> list(dict()):
     with open('storage.csv', newline='') as f:
         return list(DictReader(f))
+
+def get_data_by_cat(month=0)-> dict():
+    if month:
+        data = get_data_by_month(month)
+    else:
+        data = get_data()
+    result = dict()
+    for item in data:
+        if item['Category'] in result:
+            result[item['Category']] +=  float(item['Amount'])
+        else:
+            result[item['Category']] =  float(item['Amount'])
+    return result
+
+def get_data_by_month(month)-> list(dict()):
+    res = [item for item in get_data() if datetime.strptime(item['Date'], '%Y-%m-%d').date().month == month]
+    return res
+
+
+    
+
--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import os
-from flask import Flask, render_template, request, flash, redirect, url_for
+from flask import Flask, render_template, request
 from db import *
-import datetime
 
 def is_digit(string):
     if string.isdigit():
@@ -12,8 +10,6 @@ def is_digit(string):
             return True
         except ValueError:
             return False
-
-
 
 app = Flask(__name__)
 
@@ -26,7 +22,15 @@ def index():
         if is_digit(amount):
             add_expense(cat, date, amount)
     expenses = get_data()
-    return render_template('page.html', exps=expenses)
+    return render_template('page.html',
+                           exps=expenses,
+                           data={'Task' : 'Hours per Day'} | get_data_by_cat()
+                           )
+
+@app.route("/<month>")
+def show_by_month(month):
+    data = {'Task' : 'Hours per Day'} | get_data_by_cat(int(month))
+    return render_template('by_month.html', data = data, month=month)
 
 if __name__ == "__main__":
     app.run(debug=True)
